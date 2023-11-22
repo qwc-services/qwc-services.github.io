d477d2af1c2cc1e2528c53371eb9942a4bc7a894
Sandro Mani <manisandro@gmail.com>
2023-11-22 17:16:21 +0100

    Adapt for schema versioning change

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+branch=master
+
 echo "* Setting up venv..."
 rm -rf .venv
 python3 -mvenv .venv
@@ -11,20 +13,20 @@ echo "# References" > src/references/index.md
 echo "" >> src/references/index.md
 
 echo "* Getting latest version..."
-version=$(curl https://raw.githubusercontent.com/qgis/qwc2-demo-app/master/package.json | grep -Eo '^\s*"version": "[^"]+",$' | awk -F'"' '{print $4}')
+version=$(curl https://raw.githubusercontent.com/qgis/qwc2-demo-app/${branch}/package.json | grep -Eo '^\s*"version": "[^"]+",$' | awk -F'"' '{print $4}')
 sed "s|@version@|$version|" qwc2.yml.in > qwc2.yml
 
 echo "* Downloading plugin reference..."
-wget -q -O src/references/qwc2_plugins.md https://raw.githubusercontent.com/qgis/qwc2/master/doc/plugins.md
+wget -q -O src/references/qwc2_plugins.md https://raw.githubusercontent.com/qgis/qwc2/${branch}/doc/plugins.md
 echo "* [QWC2 plugins](qwc2_plugins.md)" >> src/references/index.md
 
 mkdir -p tmp
 echo "* Downloading schema versions..."
-wget -q -O tmp/schema-versions-master.json https://github.com/qwc-services/qwc-config-generator/raw/master/schemas/schema-versions-master.json
+wget -q -O tmp/schema-versions.json https://github.com/qwc-services/qwc-config-generator/raw/${branch}/schemas/schema-versions.json
 
 for schemaUrl in \
-    https://github.com/qwc-services/qwc-config-generator/raw/master/schemas/qwc-config-generator.json \
-    $(cat tmp/schema-versions-master.json | grep schema_url | awk -F'"' '{print $4}');
+    https://github.com/qwc-services/qwc-config-generator/raw/${branch}/schemas/qwc-config-generator.json \
+    $(cat tmp/schema-versions.json | grep schema_url | awk -F'"' '{print $4}');
 do
     service=$(basename ${schemaUrl%.json})
     echo "* Generating service schema reference for $service..."
